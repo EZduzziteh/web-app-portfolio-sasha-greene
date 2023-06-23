9e38cd6cc6b893d6f3848eceaa3791361e2ea7e5
Sasha Greene <sashagreene@hotmail.com>
2023-06-23 15:42:26 -0600

    Created Post Request

--- Pages/Admin/Blogs/Add.cshtml.cs
+++ Pages/Admin/Blogs/Add.cshtml.cs
@@ -5,8 +5,21 @@ namespace portfolio_web_app.Pages.Admin.Blogs
 {
     public class AddModel : PageModel
     {
+        [BindProperty]
+        public string Heading { get; set; }
+        [BindProperty]
+        public string PageTitle { get; set; }
+        [BindProperty]
+        public string ShortDescription { get; set; }
+        [BindProperty] 
+        public string PageContent { get; set; }
+
         public void OnGet()
         {
+        }
+
+        public void OnPost() { 
+        
         }
     }
 }
