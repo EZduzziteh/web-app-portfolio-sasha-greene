7b61776423b8ff89a4684861a399b21fa38c32a5
Sasha Greene <sashagreene@hotmail.com>
2023-06-27 20:12:30 -0600

    fixed army builder page

---
+++ Models/Domain/Unit.cs
@@ -0,0 +1,10 @@
+﻿namespace portfolio_web_app.Models.Domain
+{
+    public class Unit
+    {
+		public Guid id { get; set; }
+		public string Name { get; set; }
+        public string Points { get; set; }
+     
+    }
+}
---
+++ Pages/Admin/ArmyBuilder/AddUnit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace portfolio_web_app.Pages.Admin.ArmyBuilder
+{
+    public class AddUnitModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
